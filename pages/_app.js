feca8a9a8a8b63e1de3bbfa0e39db381b26af241
yourgotocoder <yourgotocoder@gmail.com>
2022-05-01 09:29:34 +0530

    Resolved some final issues with package-lock.json

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import Head from 'next/head';
-
-import '../styles/globals.css';
-import Layout from '../components/layout/layout';
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
-=======
 import { Provider } from 'next-auth/client';
 
 import Layout from '../components/layout/layout';
@@ -25,7 +10,6 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </Layout>
     </Provider>
->>>>>>> 12-auth
   );
 }
 
