ab2efcc022203ca960cb0da4ba58c67559cc0f29
yourgotocoder <yourgotocoder@gmail.com>
2022-05-02 14:20:33 +0530

    User profile

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,15 +1,25 @@
-import ProfileForm from './profile-form';
-import classes from './user-profile.module.css';
+import ProfileForm from "./profile-form";
+import classes from "./user-profile.module.css";
+import { useSession, getSession } from "next-auth/client";
+import { useState, useEffect } from "react";
 
 function UserProfile() {
-  // Redirect away if NOT auth
+    // Redirect away if NOT auth
+    const [isLoading, setIsLoading] = useState();
+    const [loadedSession, setLoadedSession] = useState();
 
-  return (
-    <section className={classes.profile}>
-      <h1>Your User Profile</h1>
-      <ProfileForm />
-    </section>
-  );
+    useEffect();
+    const [session, loading] = useSession();
+
+    if (loading) {
+        return <p className={classes.profile}>Loading</p>;
+    }
+    return (
+        <section className={classes.profile}>
+            <h1>Your User Profile</h1>
+            <ProfileForm />
+        </section>
+    );
 }
 
 export default UserProfile;
