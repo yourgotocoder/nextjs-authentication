66b8dceed43587b72329d9b334caf775027a673d
yourgotocoder <yourgotocoder@gmail.com>
2022-05-03 09:57:45 +0530

    Done with change password ability

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,15 +1,31 @@
-import classes from './profile-form.module.css';
+import { useRef } from "react";
+import classes from "./profile-form.module.css";
 
-function ProfileForm() {
+function ProfileForm(props) {
+  const oldPasswordRef = useRef();
+  const newPasswordRef = useRef();
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+    const enteredOldPassword = oldPasswordRef.current.value;
+    const enteredNewdPassword = newPasswordRef.current.value;
+
+    //Validation is optional
+    props.onChangePassword({
+      oldPassword: enteredOldPassword,
+      newPassword: enteredNewdPassword,
+    });
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitFormHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={newPasswordRef} />
       </div>
       <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <label htmlFor="old-password">Old Password</label>
+        <input type="password" id="old-password" ref={oldPasswordRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -5,25 +5,37 @@ import { useState, useEffect } from "react";
 
 function UserProfile(props) {
   // Redirect away if NOT auth
-//   const [isLoading, setIsLoading] = useState(true);
+  //   const [isLoading, setIsLoading] = useState(true);
 
-//   useEffect(() => {
-//     getSession().then((session) => {
-//       if (!session) {
-//         window.location.href = "/auth";
-//       } else {
-//         setIsLoading(false);
-//       }
-//     });
-//   }, []);
+  //   useEffect(() => {
+  //     getSession().then((session) => {
+  //       if (!session) {
+  //         window.location.href = "/auth";
+  //       } else {
+  //         setIsLoading(false);
+  //       }
+  //     });
+  //   }, []);
 
-//   if (isLoading) {
-//     return <p className={classes.profile}>Loading</p>;
-//   }
+  //   if (isLoading) {
+  //     return <p className={classes.profile}>Loading</p>;
+  //   }
+  const changePassword = async (passwordData) => {
+    const response = await fetch("/api/user/change-password", {
+      method: "PATCH",
+      body: JSON.stringify(passwordData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+  };
+
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm onChangePassword={changePassword} />
     </section>
   );
 }
