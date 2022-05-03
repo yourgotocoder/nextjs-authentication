f85afb11e2625aa139364ce9c3fa3f2109d0c8e2
yourgotocoder <yourgotocoder@gmail.com>
2022-05-03 08:54:44 +0530

    Resolved merge conflicts with master

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -8,30 +8,17 @@ function MainNavigation() {
 
   const logout = () => {
     signOut();
-  }
+  };
 
->>>>>>> 12-auth
   return (
     <header className={classes.header}>
       <Link href="/">
         <a>
-<<<<<<< HEAD
-          <Logo />
-=======
           <div className={classes.logo}>Next Auth</div>
->>>>>>> 12-auth
         </a>
       </Link>
       <nav>
         <ul>
-<<<<<<< HEAD
-          <li>
-            <Link href='/posts'>Posts</Link>
-          </li>
-          <li>
-            <Link href='/contact'>Contact</Link>
-          </li>
-=======
           {!session && !loading && (
             <li>
               <Link href="/auth">Login</Link>
@@ -47,7 +34,6 @@ function MainNavigation() {
               <button onClick={logout}>Logout</button>
             </li>
           )}
->>>>>>> 12-auth
         </ul>
       </nav>
     </header>
