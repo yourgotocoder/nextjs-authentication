75e634bf0d47ce67fc8b6bba32720c118a13e04d
yourgotocoder <yourgotocoder@gmail.com>
2022-05-03 08:41:57 +0530

    Added page guards using getSession from next-auth

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,25 +1,31 @@
 import ProfileForm from "./profile-form";
 import classes from "./user-profile.module.css";
-import { useSession, getSession } from "next-auth/client";
+import { getSession } from "next-auth/client";
 import { useState, useEffect } from "react";
 
-function UserProfile() {
-    // Redirect away if NOT auth
-    const [isLoading, setIsLoading] = useState();
-    const [loadedSession, setLoadedSession] = useState();
+function UserProfile(props) {
+  // Redirect away if NOT auth
+//   const [isLoading, setIsLoading] = useState(true);
 
-    useEffect();
-    const [session, loading] = useSession();
+//   useEffect(() => {
+//     getSession().then((session) => {
+//       if (!session) {
+//         window.location.href = "/auth";
+//       } else {
+//         setIsLoading(false);
+//       }
+//     });
+//   }, []);
 
-    if (loading) {
-        return <p className={classes.profile}>Loading</p>;
-    }
-    return (
-        <section className={classes.profile}>
-            <h1>Your User Profile</h1>
-            <ProfileForm />
-        </section>
-    );
+//   if (isLoading) {
+//     return <p className={classes.profile}>Loading</p>;
+//   }
+  return (
+    <section className={classes.profile}>
+      <h1>Your User Profile</h1>
+      <ProfileForm />
+    </section>
+  );
 }
 
 export default UserProfile;
